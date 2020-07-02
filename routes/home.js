e334e7a38bde63fadf78b75c8ec66564b4a5c586
Gregorius <gregeld96@gmail.com>
2020-07-02 20:03:38 +0700

    Done with session and all

--- routes/home.js
+++ routes/home.js
@@ -2,5 +2,10 @@ const controller = require('../controllers/home');
 const route = require('express').Router();
 
 route.get('', controller.read);
+route.get('/register', controller.register)
+route.get('/login', controller.login)
+route.get('/logout', controller.logout)
+route.post('/register', controller.registerPost)
+route.post('/login', controller.loginPost)
 
 module.exports = route;
