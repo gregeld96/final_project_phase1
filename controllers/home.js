fe2d4afeb1134daafdda46769b688cc6edfc112b
Gregorius <gregeld96@gmail.com>
2020-07-02 18:13:31 +0700

    Done with the rest table and design

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,11 @@
-const { Product } = require('../models');
+const { Product, Admin } = require('../models');
+const priceSeparator = require('../helpers/priceSeperator');
 
 class Controller {
     static read(req, res){
         Product.findAll({order: [["price", "DESC"]]})
             .then(data => {
-                res.render('homepage', {data});
+                res.render('homepage', {data, priceSeparator});
             })
             .catch(err => {
                 res.send(err.message);
